db1b7c3dd1272e1aebe53a9a044797540456fb0a
Daniel Galtieri <daniel.galtieri@gmail.com>
2016-02-21 10:01:18 -0600

    Added pacaur aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,6 +1,10 @@
 
 alias ls="ls --color=auto"
 
+# pacaur / pacman
+alias pud="pacaur -Syu"
+alias inst="pacaur -S"
+
 # jupyter notebooks
 alias jn="jupyter notebook"
 alias jnb="jupyter notebook --no-browser"
